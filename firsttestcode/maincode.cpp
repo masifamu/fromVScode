9534d66450411d6f2df7310b20b62ba59975da94
masifamu <asiffisanazeer@gmail.com>
2021-01-06 01:39:40 +0530

    making a distance finder function

--- firsttestcode/maincode.cpp
+++ firsttestcode/maincode.cpp
@@ -1,42 +1,31 @@
 /**
  * @file maincode.cpp
  * @author Mohammad Asif (asif@punchline.in)
- * @brief This code is somenly dedicated to testing c++ code with vscode editor
+ * @brief This code is for learning "how to use the C++ using vscode editor"
  * @version 0.1
  * @date 2021-01-06
  * 
  * @copyright Copyright (c) 2021
  * 
  */
-#include<iostream>
+#include <iostream>
+#include <cmath>
 using namespace std;
-
 /**
- * @brief the is the prototype for the function sum
+ * @brief This function finds the distance between P1(x1,y1) and P2(x2,y2)
  * 
- * @param a 
- * @param b 
- * @return int 
- */
-int sum(int a, int b);
-/**
- * @brief trying the add the comments to the main function
  * 
- * @return int 
+ * @param x1 
+ * @param y1 
+ * @param x2 
+ * @param y2 
+ * @return double
  */
-int main(){
-    cout<<"hi first code from vscode editor"<<endl;
-    cout<<"making changes for github"<<endl;
-    cout<<"sum of two numbers is "<<sum(10,20)<<endl;
-    return 0;
+double distance(double x1, double y1, double x2, double y2){
+    return sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 }
-/**
- * @brief definition of function sum
- * 
- * @param a 
- * @param b 
- * @return int 
- */
-int sum(int a, int b){
-    return(a+b);
+
+int main(){
+    cout<<"the distance between (1,1) and (2,2) is: "<<distance(1,1,2,2)<<endl;
+    return 0;
 }
